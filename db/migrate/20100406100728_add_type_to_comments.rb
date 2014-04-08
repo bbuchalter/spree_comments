92869041d5591e6a987d5c1e58c9cba2570080f2
Brian Buchalter <bal711@gmail.com>
2014-04-08 11:02:31 -0400

    Fix migrations to be compatible with MySQL 5.0
    Also optimize indexing.

--- db/migrate/20100406100728_add_type_to_comments.rb
+++ db/migrate/20100406100728_add_type_to_comments.rb
@@ -1,6 +1,7 @@
 class AddTypeToComments < ActiveRecord::Migration
   def self.up
     add_column :comments, :comment_type_id, :integer
+    add_index :comments, :comment_type_id
   end
 
   def self.down
